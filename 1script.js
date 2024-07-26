9d70efbd12923d2aedaa222e4270b597daa1174b
priyansu <routpriyanshu49@gmail.com>
2024-07-26 10:16:20 +0530

    first commit

---
+++ 1script.js
@@ -0,0 +1,78 @@
+// var a=10;
+// console.log(typeof a);
+
+// let logedIn =true
+// console.log(typeof logedIn);
+
+// let temp;
+// console.log(typeof temp);
+
+
+// let nameOn= "cool Name's is an name"
+// console.log(typeof nameOn);
+
+// non primitive
+// array[],object{},function()
+
+// let a=[10,20,30];
+// let b=a;
+// b.pop();
+
+// console.log(b);
+// console.log(a);
+
+
+
+// let nameTwo = {
+//    roll:"10",       
+//    name :"newton",
+//    age :"undefined"
+
+// };
+
+// console.log(nameTwo.age);
+
+// console.log(nameTwo["roll"]);
+
+
+// function functionName()
+// {
+//  console.log("function is calling");
+// }
+
+// functionName();
+console.log("external js");
+
+
+let a=110;
+let b=12;
+let c=a+b;
+console.log(c);
+console.log("sum of a and b"  + a*b);
+console.log(a-b);
+console.log(a/b);
+console.log(a%b);
+
+
+//urnary
+
+
+let newvalue = 120
+new value == 120
+newvalue === 120
+
+let a =a+1;
+a += 1;
+let b = b - 1;
+b += 1;
+
+
+new value == 120
+newvalue === 120
+
+100 != 120
+100!==120
+
+//let x=true;
+console.log(true && true);
+console.log(1 < 3 && 2 < 3 && 12 > 3)
---
+++ readme.md
@@ -0,0 +1,6 @@
+ what is Github?
+
+git is a opensource version control system where we store, save, share and implement our code in production level.
+Their are multiple gut tools we used but some famous tools are :- ex. github, gitlab etc 
+
+repository :- its a folder that we have created on github so that we can store our code. Simply we can say its a folder.
